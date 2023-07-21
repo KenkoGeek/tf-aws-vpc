275383ccbd580ec156b4633efc85ed8c64c743d7
Franklin Garcia <franklin.g.mendoza@gmail.com>
2023-07-21 12:30:54 -0400

    adding ~> to contraint version

--- version.tf
+++ version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "= 1.5.0"
+  required_version = "~> 1.5.0"
 
   required_providers {
     aws = {
-      version = "= 5.3.0"
+      version = "~> 5.3.0"
     }
   }
 }
