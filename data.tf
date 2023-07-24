20fdb6a425fd451fde56fb9be423cc7be60b3e25
Franklin Garcia <franklin.g.mendoza@gmail.com>
2023-07-24 16:30:16 -0400

    adding data.tf

---
+++ data.tf
@@ -0,0 +1,56 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_iam_policy_document" "cw_logs_kms_policy" {
+
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+    }
+    actions   = ["kms:*"]
+    resources = [aws_kms_key.logs_cmk.arn]
+  }
+
+  statement {
+    sid    = "Key Administrator"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.arn]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:DescribeKey"
+    ]
+    resources = [aws_kms_key.logs_cmk.arn]
+  }
+
+  statement {
+    sid    = "Use via CW Logs"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${var.aws_region}.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = [aws_kms_key.logs_cmk.arn]
+  }
+
+}
